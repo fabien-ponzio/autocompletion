a8156cbf3960d293126ed9b692fb13231aa90c3c
Fabien <fabien.ponzio@laplateforme.io>
2021-04-29 16:42:58 +0200

    fonctionnalités back done

--- element.php
+++ element.php
@@ -1,11 +1,16 @@
 <!-- afficher toutes les infos en BDD de l'élement cliqué  -->
 <?php 
 if (isset($_GET)) {
+    $response = "";
     $connexion = new \PDO('mysql:host=localhost; dbname=autocompletion; charset=utf8', 'root', ''); 
     $query = $connexion->prepare("SELECT * FROM players WHERE id = '$_GET[id]'");
     $query->execute(); 
+    
     $getPlayerInfo = $query->fetch(PDO::FETCH_ASSOC);
-    // return $getPlayerInfo;
+
+    $response .="".$getPlayerInfo['nom']." joue à l'équipe ".$getPlayerInfo['equipe']." et mesure ".$getPlayerInfo['taille']."cm.";
+
+    echo $response;    
 }
-var_dump($getPlayerInfo); 
+// var_dump($getPlayerInfo); 
 ?>
--- recherche.php
+++ recherche.php
@@ -2,12 +2,16 @@
 var_dump($_GET); 
 
 if (isset($_GET['search'])) {
+    $response = "";
     $connexion = new \PDO('mysql:host=localhost; dbname=autocompletion; charset=utf8', 'root', ''); 
     $sql = $connexion->prepare("SELECT * FROM players WHERE nom LIKE '$_GET[search]%'"); 
     $sql->execute();
     $getAll = $sql->fetchAll();
-    var_dump($sql); 
-}
 
-var_dump($getAll); 
+    for ($i=0; isset($getAll[$i]) ; $i++) { 
+        $response .="<li>".$getAll[$i]['nom']." joue à l'équipe ".$getAll[$i]['equipe']." et mesure ".$getAll[$i]['taille']."cm</li>";
+    }
+   echo $response;
+}
+// var_dump($getAll); 
 ?>
